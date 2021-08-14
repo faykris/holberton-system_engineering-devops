cefb2e216c871387f1f5cef59fba6369684bf760
faykris <faykris28@gmail.com>
2021-08-14 18:09:42 -0500

    add 100-puppet_ssh_config.pp - test with Holberton checker

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+file_line { 'Pass_Auth_no'
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
+file_line { 'Iden_File_Holberton'
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/holberton',
+}
