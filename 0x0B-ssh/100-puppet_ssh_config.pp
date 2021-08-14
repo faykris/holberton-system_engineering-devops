6e59b2edbfb83caf06df0c8a2cbb07c4975f39ef
faykris <faykris28@gmail.com>
2021-08-14 18:54:00 -0500

    update 100-puppet_ssh_config.pp - trying only with one path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # 4. Client configuration file (w/ Puppet)
 # change configuration file - connect without using a password
 exec { 'echo "    PasswordAuthentication no\n    IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config':
-  path    => '/usr/bin',
+  path => '/usr/bin',
 }
