d79f49d92b3513ecd485507a57837c3c2726e3f7
faykris <faykris28@gmail.com>
2021-09-13 17:05:52 -0500

    update 0-gather_data_from_an_API.py - fix PEP8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,19 +15,20 @@ total = 0
 name = ''
 
 for element in data_list:
-    if element['completed'] == True and element['userId'] == int(sys.argv[1]):
+    if element['completed'] is True and element['userId'] == int(sys.argv[1]):
         completed += 1
-    if element['userId'] == int(sys.argv[1]):    
+    if element['userId'] == int(sys.argv[1]):
         total += 1
 
-with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'
-        .format(sys.argv[1])) as response:
-   html2 = response.read()
+with urllib.request.urlopen(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            int(sys.argv[1]))) as response:
+    html2 = response.read()
 user = html2.decode('utf-8')
 user_dict = json.loads(user)
 name = user_dict['name']
 
-print("Employee {} is done with tasks({}/{}):".format(name, completed, total))    
+print("Employee {} is done with tasks({}/{}):".format(name, completed, total))
 for element in data_list:
-    if element['completed'] == True and element['userId'] == int(sys.argv[1]):
+    if element['completed'] is True and element['userId'] == int(sys.argv[1]):
         print("\t {}".format(element['title']))
