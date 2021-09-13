a721d2d701087b4573a9c922262bc964db20e721
faykris <faykris28@gmail.com>
2021-09-13 17:32:45 -0500

    update 0-gather_data_from_an_API.py - add if condition when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,34 +4,34 @@ import json
 import sys
 import urllib.request
 
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            'https://jsonplaceholder.typicode.com/todos') as response:
+        html1 = response.read()
+    data = html1.decode('utf-8')
+    data_list = json.loads(data)
 
-with urllib.request.urlopen(
-        'https://jsonplaceholder.typicode.com/todos') as response:
-    html1 = response.read()
-data = html1.decode('utf-8')
-data_list = json.loads(data)
+    completed = 0
+    total = 0
+    name = ''
 
-completed = 0
-total = 0
-name = ''
+    for element in data_list:
+        if element.get('completed') is True and element.get('userId') \
+                == int(sys.argv[1]):
+            completed += 1
+        if element.get('userId') == int(sys.argv[1]):
+            total += 1
 
-for element in data_list:
-    if element.get('completed') is True and element.get('userId') \
-            == int(sys.argv[1]):
-        completed += 1
-    if element.get('userId') == int(sys.argv[1]):
-        total += 1
+    with urllib.request.urlopen(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(
+                int(sys.argv[1]))) as response:
+        html2 = response.read()
+    user = html2.decode('utf-8')
+    user_dict = json.loads(user)
+    name = user_dict.get('name')
 
-with urllib.request.urlopen(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            int(sys.argv[1]))) as response:
-    html2 = response.read()
-user = html2.decode('utf-8')
-user_dict = json.loads(user)
-name = user_dict.get('name')
-
-print("Employee {} is done with tasks({}/{}):".format(name, completed, total))
-for element in data_list:
-    if element.get('completed') is True and element.get('userId') ==\
-            int(sys.argv[1]):
-        print("\t {}".format(element.get('title')))
+    print("Employee {} is done with tasks({}/{}):".format(name, completed, total))
+    for element in data_list:
+        if element.get('completed') is True and element.get('userId') ==\
+                int(sys.argv[1]):
+            print("\t {}".format(element.get('title')))
