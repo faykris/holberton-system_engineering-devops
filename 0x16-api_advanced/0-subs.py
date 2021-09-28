6b4d4058008d01e6294df2d88b4a52bbe8868a00
faykris <faykris28@gmail.com>
2021-09-27 19:59:09 -0500

    update 0-subs.py: adding tabs on with command

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,16 +3,16 @@
 
 
 def number_of_subscribers(subreddit):
-    """ number_of_subscribers - retrieve aa number of subscribers"""
+    """number_of_subscribers - retrieve aa number of subscribers"""
     import requests
 
-    with requests.get(
+    response = requests.get(
             "https://www.reddit.com/r/{}/about.json".format(subreddit),
             headers={"User-Agent": "Mozilla",
                      "Content-Type": "application/json"},
             allow_redirects=False,
-    ) as response:
-        if response.status_code > 299:
-            return 0
-        n_subscribers = response.json()
-        return n_subscribers.get("data").get("subscribers")
+    )
+    if response.status_code > 299:
+        return 0
+    n_subscribers = response.json()
+    return n_subscribers.get("data").get("subscribers")
