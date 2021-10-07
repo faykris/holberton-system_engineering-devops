6e454930b756ef3fafe96b7a795897aa9a0e1373
faykris <faykris28@gmail.com>
2021-10-07 09:45:28 -0500

    update 1-user_limit.pp: fix identation on command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Fix failed requests from 962 to 0 - change worker_processes value: 4 to 7
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf; sudo service nginx restart",
+  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf; sudo service nginx restart",
   provider => shell,
 }
