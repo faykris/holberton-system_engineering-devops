015efceb94495de382edbc6554fde9129b3b4d53
faykris <faykris28@gmail.com>
2021-10-06 20:43:18 -0500

    add 1-user_limit.pp: waiting for Hoberton checker

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# Fix holberton user limits - comment two lines which says holberton 
+exec { 'change-os-configuration-for-holberton-user':
+  command => "sed -i 's/holberton/# holberton/'g /etc/security/limits.conf",
+  provider => shell,
+}
